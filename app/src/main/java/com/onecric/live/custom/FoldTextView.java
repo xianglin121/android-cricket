75da3901543b283ea8ba7b0b5ee1ab71729a88f4
xianglin121 <838277935@qq.com>
2023-01-08 16:30:22 +0800

    1

--- app/src/main/java/com/onecric/live/custom/FoldTextView.java
+++ app/src/main/java/com/onecric/live/custom/FoldTextView.java
@@ -216,7 +216,7 @@ public class FoldTextView extends AppCompatTextView {
                 StringBuilder builder = new StringBuilder(ELLIPSIZE_END).append(mFoldText);
                 end -= paint.breakText(mOriginalText, start, end, false, paint.measureText(builder.toString()), null);
                 float x = getWidth() - getPaddingLeft() - getPaddingRight() - getTextWidth(mFoldText);
-                while (layout.getPrimaryHorizontal(end - 1) + getTextWidth(mOriginalText.subSequence(end - 1, end).toString()) < x) {
+                while (end <= mOriginalText.length() && layout.getPrimaryHorizontal(end - 1) + getTextWidth(mOriginalText.subSequence(end - 1, end).toString()) < x) {
                     end++;
                 }
                 end--;
