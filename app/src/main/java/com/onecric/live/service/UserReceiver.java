70757bc8af537889623b82b29891230d24fdcb7a
xl121 <xhl19860804>
2023-02-20 19:12:52 +0800

    1

--- app/src/main/java/com/onecric/live/service/UserReceiver.java
+++ app/src/main/java/com/onecric/live/service/UserReceiver.java
@@ -13,6 +13,7 @@ import com.engagelab.privates.push.api.MobileNumberMessage;
 import com.engagelab.privates.push.api.NotificationMessage;
 import com.engagelab.privates.push.api.PlatformTokenMessage;
 import com.onecric.live.activity.AboutUsActivity;
+import com.onecric.live.activity.CricketDetailActivity;
 import com.onecric.live.activity.LiveDetailActivity;
 import com.onecric.live.common.ExampleGlobal;
 import com.onecric.live.listener.StatusObserver;
@@ -84,17 +85,23 @@ public class UserReceiver extends MTCommonReceiver {
     @Override
     public void onNotificationClicked(Context context, NotificationMessage notificationMessage) {
         ExampleLogger.i(TAG, "onNotificationClicked:" + notificationMessage.toString());
-        // 用于演示消息展示
-        Intent intent = new Intent();
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        intent.setClass(context, LiveDetailActivity.class);
-        Bundle bundle = notificationMessage.getExtras();
-        if (bundle != null) {
-            intent.putExtra("anchorId", bundle.getInt("anchorId"));
-            intent.putExtra("type", bundle.getInt("type"));
-            intent.putExtra("matchId", bundle.getInt("matchId"));
-        }
-        context.startActivity(intent);
+        // 用于演示消息展示  todo 等接口定好后再来处理这里点击消息的跳转逻辑
+//        Intent intent = new Intent();
+//        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//        Bundle bundle = notificationMessage.getExtras();
+//        if (bundle != null && bundle.getInt("matchStatus") == 1) {//比赛开始   进入视频直播界面
+//            intent.setClass(context, LiveDetailActivity.class);
+//            intent.putExtra("anchorId", bundle.getInt("anchorId"));
+//            intent.putExtra("type", bundle.getInt("type"));
+//            intent.putExtra("matchId", bundle.getInt("matchId"));
+//            context.startActivity(intent);
+//        } else if (bundle != null && bundle.getInt("matchStatus") == 0) {//比赛已经结束 进入比赛详情界面
+//            intent.setClass(context, CricketDetailActivity.class);
+//            intent.putExtra("matchId", bundle.getInt("matchId"));
+//            context.startActivity(intent);
+//            context.startActivity(intent);
+//        }
+//        context.startActivity(intent);
     }
 
     /**
